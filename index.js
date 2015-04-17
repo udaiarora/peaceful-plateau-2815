14073b6ba8a2f9f7954e97305902ad6f62164e9b
Udai Arora <rocketmailudai@gmail.com>
2015-04-17 01:18:03 -0700

    Improved Imagename timestapms

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ app.use(express.static(__dirname + '/public', { maxAge: 7200000 }));
 var imagename=1;
 var ext=".jpg";
 function getimage() {
+	imagename=parseInt(1 + ((new Date().getTime()-1429254000000)/86400000));
+	imagename=imagename>64?1:imagename;
 	return {bgurl:imagename+ext};
 }
-setInterval(function(){imagename=imagename>64?1:imagename+1}, 86400000);
+setInterval(function(){imagename=imagename>64?1:imagename+1}, );
+
+
 
 app.get('/', function(request, response) {
   response.send(getimage());
